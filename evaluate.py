6dd4dad1eec7b179d93d693cf1d9b71f99b3bc4f
bob-lytton <leiyunton@pku.edu.cn>
2020-03-26 20:23:16 +0800

    show results

--- evaluate.py
+++ evaluate.py
@@ -9,15 +9,21 @@ def metrics(model, test_loader, top_k):
     hit_list    = []
     ndcg_list   = []
     eval_len    = []
+    ranklists = []
+    predictions = []
     for i, data in enumerate(test_loader):
         users, items, labels = data
         users = users.cuda()
         items = items.cuda()
         labels = labels.cuda()
         prediction_i, _ = model(users, items, labels)
+
+        
         values, indices = torch.topk(prediction_i, top_k)
         ranklist = indices[0].tolist()
-        # print(ranklist)
+
+        ranklists.append(ranklist)
+        predictions.append(prediction_i)
         gtItems = torch.nonzero(labels)[:, 1].tolist()
         prec_list.append(getP(ranklist, gtItems))
         recall_list.append(getR(ranklist, gtItems))
@@ -25,6 +31,8 @@ def metrics(model, test_loader, top_k):
         ndcg_list.append(getNDCG(ranklist, gtItems))
         eval_len.append(len(gtItems))
 
+    print("prediction of the last user:", predictions[-1])
+    print("output of the last user:", ranklists[-1])
     prec = np.mean(np.asarray(prec_list))
     recall = np.mean(np.asarray(recall_list))
     hit_ratio = np.sum(hit_list)/np.sum(eval_len)
